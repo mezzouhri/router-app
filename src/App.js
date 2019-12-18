a07202f9f7b6547bf5705d69e33b2a0496eef30a
Mohcine EZZOUHRI <ezzouhri.dev@gmail.com>
2019-12-18 17:09:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div>
+          <Switch>
+            <Route path="/products/:id" component={ProductDetails} />
+            <Route path="/products" component={Products} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/admin" component={Dashboard} />
+            <Route path="/NotFound" component={NotFound} />
+            <Route path="/" component={Home} />
+            <Redirect to="/NotFound" />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
